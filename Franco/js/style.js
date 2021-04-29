dd8b87d2eea03dc26389d61667614a2a4172aaea
Nguyen Ly <voicoia1k6@gmail.com>
2021-04-29 10:54:13 +0700

    2904

--- Franco/js/style.js
+++ Franco/js/style.js
@@ -1,45 +1,60 @@
-function search() {
-	var x = $('.header-menu-search');
-	x.addClass('seach__active');
-}
-function Cart() {
-  var y = $('.header-mini-cart');
-  y.addClass('mini-cart-active')
-}
+$(document).ready(function(){
+
+  $('.dot3').click(function(e){
+    e.preventDefault();
+    var x = $('.header-menu-search');
+    x.addClass('seach__active');
+  });
+
+  $('.Cart').click(function(e){
+    e.preventDefault();
+    var y = $('.header-mini-cart');
+    y.addClass('mini-cart-active')
+  });
+
+  var a= $('.blog-container').width();
+      var tam =a;
+      $(".point-two").click(function(){
+        tam+=a;
+        $('.blog-container').css('margin-left', '-'+tam+'px');
+      });
+
+
+});
 function cls() {
-	var x = $('.header-menu-search');
-	x.removeClass('seach__active');
+ var x = $('.header-menu-search');
+ x.removeClass('seach__active');
 }  
 function clscart() {
   var x = $('.header-mini-cart');
   x.removeClass('mini-cart-active');
 }
 $( function() {
-    var availableTags = [
-      "ActionScript",
-      "AppleScript",
-      "Asp",
-      "BASIC",
-      "C",
-      "C++",
-      "Clojure",
-      "COBOL",
-      "ColdFusion",
-      "Erlang",
-      "Fortran",
-      "Groovy",
-      "Haskell",
-      "Java",
-      "JavaScript",
-      "Lisp",
-      "Perl",
-      "PHP",
-      "Python",
-      "Ruby",
-      "Scala",
-      "Scheme"
-    ];
-    $( "#tk" ).autocomplete({
-      source: availableTags
-    });
-  } );
+  var availableTags = [
+  "ActionScript",
+  "AppleScript",
+  "Asp",
+  "BASIC",
+  "C",
+  "C++",
+  "Clojure",
+  "COBOL",
+  "ColdFusion",
+  "Erlang",
+  "Fortran",
+  "Groovy",
+  "Haskell",
+  "Java",
+  "JavaScript",
+  "Lisp",
+  "Perl",
+  "PHP",
+  "Python",
+  "Ruby",
+  "Scala",
+  "Scheme"
+  ];
+  $( "#tk" ).autocomplete({
+    source: availableTags
+  });
+} );
