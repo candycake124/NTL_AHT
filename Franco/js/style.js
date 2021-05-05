8a0f1e5b7c4ccb3a3bc499b7210eb578679567b6
Nguyen Ly <voicoia1k6@gmail.com>
2021-05-05 11:05:54 +0700

    Reponsive

--- Franco/js/style.js
+++ Franco/js/style.js
@@ -1,25 +1,53 @@
 $(document).ready(function(){
-
+//---------------------------------------------search-------------
   $('.dot3').click(function(e){
     e.preventDefault();
     var x = $('.header-menu-search');
     x.addClass('seach__active');
   });
-
+//-------------------------------------------mini-cart----------------
   $('.Cart').click(function(e){
     e.preventDefault();
     var y = $('.header-mini-cart');
     y.addClass('mini-cart-active')
   });
 
-  var a= $('.blog-container').width();
-      var tam =a;
-      $(".point-two").click(function(){
-        tam+=a;
-        $('.blog-container').css('margin-left', '-'+tam+'px');
-      });
+//-----------------------------------------slider----------------------
+  var a= $('.slider li').width();
+  var margin;
+  if ($( window ).width()< 321 ) {
+    margin=10;
+  }
+  else if ($( window ).width()< 1025) {
+    margin=15;
+  }
+  else margin=20 + 10;
+  
+  $(".point-one").click(function(e){
+    e.preventDefault();
+    $('.blog-list').css('margin-left', '0px');
+  });
 
 
+  $(".point-two").click(function(e){
+    var tam2=a*3+margin*2;
+    e.preventDefault();
+    $('.blog-list').css('margin-left','-'+tam2+'px');
+  });
+
+
+  $(".bbbbbb").click(function(e){
+    e.preventDefault();
+    var tam3=a*3+margin*2;
+    $('.blog-list').css('margin-left', '-'+tam3*2+'px');
+  });
+
+//-----------------------------------------menu-bar---------------------
+  $('.menubarrrrr').click(function(e){
+    e.preventDefault();
+    var header_menu = $(".header-menu-menu");
+    header_menu.addClass('menu_man1');
+  });
 });
 function cls() {
  var x = $('.header-menu-search');
@@ -44,15 +72,7 @@ $( function() {
   "Fortran",
   "Groovy",
   "Haskell",
-  "Java",
-  "JavaScript",
-  "Lisp",
-  "Perl",
-  "PHP",
-  "Python",
-  "Ruby",
-  "Scala",
-  "Scheme"
+  "Java"
   ];
   $( "#tk" ).autocomplete({
     source: availableTags
